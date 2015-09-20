a18f31e4e47a7ab9a3b11573eb7d7b457a508936
G-Square <746535@gmail.com>
2015-09-20 17:51:02 -0400

    minor html changes

--- about.php
+++ about.php
@@ -158,9 +158,6 @@
 		<!-- jQuery -->
 		<script src="js/jquery.js"></script>
 
-		<!-- Bootstrap Core JavaScript -->
-		<script src="js/bootstrap.min.js"></script>
-
 		<!-- Menu Toggle Script -->
 		<script>
 		$("#menu-toggle").click(function(e) {
