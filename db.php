6f4477500a70180007918084a9cfd0e81f74b53c
G-Square <746535@gmail.com>
2015-09-20 00:00:13 -0400

    mod

--- db.php
+++ db.php
@@ -1,18 +1,3 @@
-// <?php
-// $link = mysql_connect("mysql5.000webhost.com" , "a2199089_CIS485", "12345_CIS485");
-
-// if($link == false) {
-//   echo "Error: can't connect to database server";
-//   exit;
-// }
-
-// if(mysql_select_db("a2199089_CIS485", $link) == false) {
-//   echo "Error: can't connect to database";
-//   exit;
-// }
-// ?>
-
-
 <?php
 //very important line without it, .env will not work
 require_once 'vendor/autoload.php';
