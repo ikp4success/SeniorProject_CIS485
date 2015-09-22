6e102fc15332a62adc17aa5919e5d1d9ed64e058
G-Square <746535@gmail.com>
2015-09-22 04:54:40 -0400

    reverted to old style of html

--- student_profile.php
+++ student_profile.php
@@ -1,15 +1,90 @@
 <html lang="en">
 <head>
-	<title>Clicker App Student Users Page</title>
-	<?php
-	include("views/sidebar-head.php");
-	?>
+	<title>Clicker App Student Profile</title>
+	<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+	<link href='css/style.css' rel='stylesheet' type='text/css'>
+	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script> -->
+	<link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+	<!-- gen.js needs to make sure jquery is loaded to work-->
+	<script src="js/gen.js"></script>
+	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<link href="css/simple-sidebar.css" rel="stylesheet" media="all" />
+	<link href="css/profilestyle.css" rel="stylesheet" media="all" />
 </head>
 
 <body style="background-color:#D8D8D8;">
-<?php
-include("views/sidebar-body.php");
-?>
+		<div id="container">
+			<div id="wrapper">
+
+				<!-- Sidebar -->
+				<div id="sidebar-wrapper">
+					<ul class="sidebar-nav">
+						<li style="background-color:#045FB4;">
+							<a style="color:#FFFFFF;" href="student_profile.php"> 	<div class="formatbar">
+								<img src="images/student-32.png">
+							 Welcome, <?php
+						session_start();
+
+						if (isset($_SESSION['reg_username'])) {
+							echo  $_SESSION['reg_username'];
+						}
+						?>
+							</div></a>
+						</li>
+						<li>
+							<a href="student_home.php" title="student home">
+								<div class="formatbar">
+								<img src="images/home.png">  Student Home </div>
+							</a>
+
+						</li>
+						<li>
+
+							<a href="#">
+								<div class="formatbar" title="questions">
+								<img src="images/question.png">  Questions</div>
+							</a>
+
+						</li>
+						<li>
+
+							<a href="#">
+								<div class="formatbar" title="answers">
+								<img src="images/book.png">  Answers </div></a>
+
+						</li>
+						<li>
+
+							<a href="#" title="grades"><div class="formatbar">
+								<img src="images/letter.png">  Grades </div></a>
+
+						</li>
+						<li>
+
+							<a href="about.php" title="about us"><div class="formatbar">
+								<img src="images/about.png">  About	</div></a>
+
+						</li>
+
+							<li>
+
+								<a href="exit.php" title="Logout">
+									<div class="formatbar">
+								<img src="images/exit.png">  LogOff </div></a>
+
+							</li>
+
+
+
+					</ul>
+					<div class="sidebar-nav-bottom">
+							<p class="sidebar-copyright">&copy; 2015 Senior Project CIS485 Clicker App. <a href="mailto:CIS485@csuohio.com">Contact</a></p>
+						</div>
+				</div>
+				<!-- /#sidebar-wrapper -->
+
 				<!-- Page Content -->
 				<div id="page-content-wrapper">
 					<div class="col-lg-12">
@@ -35,7 +110,7 @@ include("views/sidebar-body.php");
 									$reg_user = $_SESSION['reg_username'];
 
 									$sql  = "SELECT reg_username, reg_gender,reg_fullname,reg_email
-											 FROM student_users WHERE reg_username = '$reg_user'";
+											  FROM student_users WHERE reg_username = '$reg_user'";
 
 									$result = $link->query($sql);
 
@@ -84,6 +159,16 @@ include("views/sidebar-body.php");
 		</div>
 		<!-- /#wrapper -->
 
+		<!-- Bootstrap Core JavaScript -->
+		<script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+
+		<!-- Menu Toggle Script -->
+		<script>
+		$("#menu-toggle").click(function(e) {
+			e.preventDefault();
+			$("#wrapper").toggleClass("toggled");
+		});
+		</script>
 
 
 
