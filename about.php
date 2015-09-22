534cae0c672596a5c70f2ffd421f9b6ffd00177b
G-Square <746535@gmail.com>
2015-09-22 04:54:03 -0400

    reverted to old style of html

--- about.php
+++ about.php
@@ -1,14 +1,99 @@
 <html lang="en">
 <head>
-<?php
-include ("views/sidebar-head.php");
-?>
 	<title>Clicker App About Page</title>
-
+	<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+	<link href='css/style.css' rel='stylesheet' type='text/css'>
+	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script> -->
+	<link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+	<!-- gen.js needs to make sure jquery is loaded to work-->
+	<script src="js/gen.js"></script>
+	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<link href="css/simple-sidebar.css" rel="stylesheet" media="all" />
+	<link href="css/profilestyle.css" rel="stylesheet" media="all" />
 </head>
 
-<?php include ("views/sidebar-body.php");
-?>
+	<body style="background-color:#D8D8D8;">
+		<div id="container">
+			<div id="wrapper">
+
+				<!-- Sidebar -->
+				<div id="sidebar-wrapper">
+					<ul class="sidebar-nav">
+						<li style="background-color:#045FB4;"><a style="color:#FFFFFF;" href="student_profile.php"> 	<div class="formatbar">
+								<img src="images/student-32.png">
+							 Welcome, <?php
+						session_start();
+
+						if (isset($_SESSION['reg_username'])) {
+							echo  $_SESSION['reg_username'];
+						}
+						?>
+							</div></a></li>
+					</li>
+						<li>
+
+
+							<a href="student_home.php">
+								<div class="formatbar">
+								<img src="images/home.png">
+								 Student Home
+								</div>
+							</a>
+
+						</li>
+						<li>
+
+							<a href="#">
+								<div class="formatbar">
+								<img src="images/question.png">
+								 Questions
+								</div>
+							</a>
+
+						</li>
+						<li>
+
+							<a href="#">
+								<div class="formatbar">
+								<img src="images/book.png">
+								  Answers</div></a>
+
+						</li>
+						<li>
+
+							<a href="#"><div class="formatbar">
+								<img src="images/letter.png">  Grades</div></a>
+
+						</li>
+						<li class="sidebar-brand">
+
+							<a href="about.php"><div class="formatbar">
+								<img src="images/about.png">  About	</div></a>
+
+						</li>
+
+							<li>
+
+								<a href="exit.php" title="Login private area">
+									<div class="formatbar">
+								<img src="images/exit.png">
+									  LogOff
+										</div>
+								</a>
+
+							</li>
+
+
+
+					</ul>
+					<div class="sidebar-nav-bottom">
+							<p class="sidebar-copyright">&copy; 2015 Senior Project CIS485 Clicker App. <a href="mailto:CIS485@csuohio.com">Contact</a></p>
+						</div>
+				</div>
+				<!-- /#sidebar-wrapper -->
+
 				<!-- Page Content -->
 				<div id="page-content-wrapper">
 					<div class="col-lg-12">
