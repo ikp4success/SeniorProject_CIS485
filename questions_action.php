1e4191af68a942580f6af0563a2b16aa542ce685
G-Square <746535@gmail.com>
2015-09-27 19:52:25 -0400

    test qa db

--- questions_action.php
+++ questions_action.php
@@ -1,6 +1,5 @@
 <?php
-
-//include "db.php";
+include "db.php";
 
     //get question and answers and correct answer from form
     $question = $_POST["reg_username"];
@@ -10,9 +9,11 @@
     $multi_choices3 = $_POST["multi-choices3"];
     $multi_choices4 = $_POST["multi-choices4"];
 
-$prof_username = $_SESSION["reg_username"];
-$sql = "INSERT techer_id= '$teacher_id' question='$question' a1='$multi_choices1' a2='$question2' a3='$question3' a4='$question4' correct='$correct'
-        FROM questions_answers";
+$prof_id = $_SESSION["reg_id"];
+
+$sql =
+    "INSERT INTO questions_answers (techer_id, question, a1,a2,a3,a4, correct)
+    VALUES('$prof_id', $question', '$multi_choices1','$multi_choices2','$multi_choices3','$multi_choices4','$correct')";
 
 echo $sql;
 return $link = new mysqli(getenv('MYSQL_DB_HOST'), getenv('MYSQL_DB_USERNAME'), getenv('MYSQL_DB_PASSWORD_CIS'), 'clickerapp') or
