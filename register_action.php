a3a7a57771ef3c07bc5b1de94046c468f6ad12c4
G-Square <746535@gmail.com>
2015-10-15 16:54:09 -0400

    minor changes

--- register_action.php
+++ register_action.php
@@ -1,48 +1,39 @@
-<?php
-include "db.php";
-session_start();
+<?php 
+	session_start();
+	$reg_username = $_POST["reg_username"];
+	$reg_email = $_POST["reg_email"];
+	$reg_password = md5($_POST["reg_password"]);
+	$reg_password_confirm = md5($_POST["reg_password_confirm"]);
+	$fname = $_POST["reg_fname"];
+	$lname = $_POST["reg_lname"];
+	$id = $_POST["school_id"];
+	$admin_yn = 'N';
 
-$reg_username = $_POST["reg_username"];
-$reg_email = $_POST["reg_email"];
-$reg_password = md5($_POST["reg_password"]);
-$reg_password_confirm = md5($_POST["reg_password_confirm"]);
-$reg_fullname = $_POST["reg_fullname"];
-$reg_gender = $_POST["reg_gender"];
-//input checks
+	if ($reg_password != $reg_password_confirm) {
+	  echo '<a href="register.php">Error: passwords do not match. Try again!</a>';
+	  exit;
+	}
+	
+	//$pos = strpos($id, '1');
+	$x = $id[0];
+	if($x == '1') {
+		$admin_yn = 'Y';
+	}
+	
+	
+	
+	include "db.php";
+	
+	$sql = "INSERT INTO users (id, reg_username, f_name, l_name, reg_password, admin_yn, reg_email) 
+	VALUES('$id', '$reg_username', '$fname', '$lname', '$reg_password', '$admin_yn', '$reg_email')";
+	$result = $link->query($sql);
 
-
-if ($reg_password != $reg_password_confirm) {
-  echo '<a href="register.php">Error: password does not match. Try again!</a>';
-  exit;
-}
-
-
-
-$link=connectDb('cis485');
-/*
-$verf_link=
-// using SendGrid's PHP Library - https://github.com/sendgrid/sendgrid-php
-$sendgrid = new SendGrid($api_user, $api_key);
-$email    = new SendGrid\Email();
-
-$email->addTo($email)
-    ->setFrom("csuclicker@DoNotReply.com")
-    ->setSubject("You'r accapted to CSU-Clicker")
-    ->setHtml("Hi $reg_fullname /n got to $verf_link");
-
-$sendgrid->send($email);
-*/
-$sql = "INSERT INTO student_users (reg_username, reg_fullname, reg_gender, reg_email, reg_password)
-        VALUES('$reg_username', '$reg_fullname','$reg_gender','$reg_email', '$reg_password' )";
-
-$result = $link->query($sql);
-
-
-if($result === false)
-  echo '<a href="register.php">Error: cannot execute query</a>';
-else
-  header("Location: login.php");
-
-//mysql_close($link);
-exit;
+	if($result == false)
+	  echo '<a href="register.php">Error: cannot execute query</a>';
+	else {
+	  echo "<SCRIPT> alert('Registration Successful'); </SCRIPT>";
+	  header("Location: login.php");
+	}
+	//mysql_close($link);
+	exit;
 ?>
